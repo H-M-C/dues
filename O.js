3f1bf8746e0e18e54f929fbac725bf19e3dcf6e8
H-M-C <90295714+H-M-C@users.noreply.github.com>
2021-09-19 18:02:20 +0100

    Update O.js

--- O.js
+++ O.js
@@ -1,5 +1,9 @@
 //alert("hello guys.");
 //alert("from GitHub");
+window.onload = function (){
+  var elem = document.getElementById("see");
+  elem.innerHTML = "2400";
+}
 $("#see").text(2400);
 function adam(){
   alert("WELCOME TO UNIUYO DUES PORTAL");
