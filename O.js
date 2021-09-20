a4c70c0e07494f6a62a3c6c20aeb5f00e4bc2028
H-M-C <90295714+H-M-C@users.noreply.github.com>
2021-09-20 06:06:00 +0100

    Update O.js

--- O.js
+++ O.js
@@ -4,9 +4,9 @@ window.onload = function (){
   var elem = document.getElementById("see");
   elem.innerHTML = "2400";
   
-		var st = '<script src="https://checkout.flutterwave.com/v3.js"/>';
+	var st = '<script src="https://checkout.flutterwave.com/v3.js"/>';
 	var tx_ref = 'UDIN163208691699928';
-	var __cast = document.getElementsByClassName("controls");
+	var __cast = document.getElementsByClassName("control-group");
 	__cast[0].innerHTML = ' ';
 }
 //$("#see").text(2400);
